abec7b8cc84c8bd9225f78134737dd5c5e68a32e
repl.it user <replituser@example.com>
2020-11-06 17:36:53 +0000

    Assginment 5

--- Life.java
+++ Life.java
@@ -1,18 +1,11 @@
 public class Life implements ILife {
-  public static final int ZELLENANZAHL = 600;
-  public static boolean[][] zelle = new boolean[ZELLENANZAHL][ZELLENANZAHL];
-  int startzellen = 100;
-  static int gen = 0;
-  
+
+  boolean[][] generations = null;
+
   public static void main(String[] args) {
-    Life l = new Life(new String[] {  ".....",
-                                      ".....",
-                                      ".***.",
-                                      ".....",
-                                      "....." });
+    Life l = new Life(new String[] { ".....", ".....", ".***.", ".....", "....." });
     l = (Life) l.nextGeneration();
   }
-
 
   public Life() {
     nukeAll();
@@ -20,68 +13,102 @@ public class Life implements ILife {
 
   public Life(String[] setup) {
     this();
+    generations = new boolean[setup.length][setup[0].length()];
     for (int y = 0; y < setup.length; y++)
       for (int x = 0; x < setup[y].length(); x++)
         if (setup[y].charAt(x) != '.')
           setAlive(x, y);
+        else
+          setDead(x, y);
+
   }
 
+  public Life(boolean[][] nextGeneration){
+    this();
+    generations = nextGeneration;
+  }
 
   @Override
   public void nukeAll() {
-  
-
+    // TODO Auto-generated method stub
+    if (generations == null)
+      return;
+    for (int y = 0; y < generations.length; y++) {
+      int lineLength = generations[y].length;
+      for (int x = 0; x < lineLength; x++) {
+        generations[x][y] = false;
+      }
+    }
   }
 
   @Override
   public void setAlive(int x, int y) {
-    if (neighbours(x, y)==3 && !zelle[x][y]){
-      zelle[x][y] = true;
-    }
+    // TODO Auto-generated method stub
+    generations[x][y] = true;
+
   }
 
   @Override
   public void setDead(int x, int y) {
-    if(neighbours(x, y)>3){
-      zelle [x][y] = false;
-    }
+    // TODO Auto-generated method stub
+    generations[x][y] = false;
 
   }
 
   @Override
   public boolean isAlive(int x, int y) {
-    if(neighbours(x, y)==2 || neighbours(x, y)==3){
-      //Zelle bleibt unverändert
+    // TODO Auto-generated method stub
+    if (y < 0 || x < 0)
+      return false;
+    if (y >= generations.length || x >= generations[0].length) {
+      return false;
     }
-    return false;
+    return generations[x][y];
   }
 
   @Override
   public ILife nextGeneration() {
-    gen++;
-    System.out.println("Generation "+ gen);
-
-    for(int x=0; x<ZELLENANZAHL; x++){
-      for(int y=0; y<ZELLENANZAHL; y++){
-        int n = neighbours(x, y);
+    boolean[][] nextGeneration = new boolean[generations.length][generations[0].length];
+    for (int y = 0; y < generations.length; y++) {
+      int lineLength = generations[y].length;
+      for (int x = 0; x < lineLength; x++) {
+        int num = numLivingNeighbours(x, y);
+        if (isAlive(x, y)) {
+         
+          if (num == 2 || num == 3)
+            nextGeneration[x][y] = true;
+          else
+            nextGeneration[x][y] = false;
+        }else{
+          if(num == 3)
+            nextGeneration [x][y] = true;
+          else
+            nextGeneration [x][y] = false;
+          
+        }
       }
     }
     return null;
   }
 
-  public static int neighbours(int x, int y){
-    int count = 0;
-
-    int[] xoff = {1,1,0,-1,-1,-1,0,1};
-    int[] yoff = {0,1,1,1,0,-1,-1,-1};
-
-    for(int i=0; i<8; i++){
-      try{
-        if(zelle[x+xoff[i]][y+yoff[i]]){ count++;}
-      }catch(Exception e){
-        e.printStackTrace();
-      }
-    }
-    return count;
+  public int numLivingNeighbours(int x, int y) {
+    int counter = 0;
+    if (isAlive(x, y - 1))
+      counter++;
+    if (isAlive(x + 1, y - 1))
+      counter++;
+    if (isAlive(x + 1, y))
+      counter++;
+    if (isAlive(x + 1, y + 1))
+      counter++;
+    if (isAlive(x, y + 1))
+      counter++;
+    if (isAlive(x - 1, y + 1))
+      counter++;
+    if (isAlive(x - 1, y))
+      counter++;
+    if (isAlive(x - 1, y - 1))
+      counter++;
+    return counter;
   }
 }
