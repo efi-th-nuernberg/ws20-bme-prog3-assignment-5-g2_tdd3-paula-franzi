151509c6bae875c13f53028f0aa361b7ba1d9508
Thomas Kammerer <thomas.kammerer@th-nuernberg.de>
2020-11-13 17:45:57 +0100

    Feedback (#1)
    merging commits

--- Life.java
+++ Life.java
@@ -1,55 +1,121 @@
 public class Life implements ILife {
-  
+
+  boolean[][] generations = null;
+
   public static void main(String[] args) {
-    Life l = new Life(new String[] {  "     ",
-                                      "     ",
-                                      " *** ",
-                                      "     ",
-                                      "     " });
-    l = (Life) l.nextGeneration();
+    Life l = new Life(new String[] { ".....", ".....", ".***.", ".....", "....." });
+    // l = (Life) l.nextGeneration();
   }
 
-
   public Life() {
+
+    generations = new boolean[5][5];
     nukeAll();
   }
 
   public Life(String[] setup) {
     this();
+
+    generations = new boolean[setup.length][setup[0].length()];
     for (int y = 0; y < setup.length; y++)
       for (int x = 0; x < setup[y].length(); x++)
-        if (setup[y].charAt(x) != ' ')
+        if (setup[y].charAt(x) != '.')
           setAlive(x, y);
+        else
+          setDead(x, y);
+
   }
 
+  public Life(boolean[][] nextGeneration) {
+
+    this();
+
+    generations = nextGeneration;
+  }
 
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
-
+    if (generations == null)
+      return;
+    for (int y = 0; y < generations.length; y++) {
+      int lineLength = generations[y].length;
+      for (int x = 0; x < lineLength; x++) {
+        generations[x][y] = false;
+      }
+    }
   }
 
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
+    generations[x][y] = true;
 
   }
 
   @Override
   public void setDead(int x, int y) {
     // TODO Auto-generated method stub
+    generations[x][y] = false;
 
   }
 
   @Override
   public boolean isAlive(int x, int y) {
     // TODO Auto-generated method stub
-    return false;
+    if (y < 0 || x < 0)
+      return false;
+    if (y >= generations.length || x >= generations[0].length) {
+      return false;
+    }
+    return generations[x][y];
   }
 
   @Override
   public ILife nextGeneration() {
-    // TODO Auto-generated method stub
-    return null;
+
+    boolean[][] nextGeneration = new boolean[generations.length][generations[0].length];
+    for (int y = 0; y < generations.length; y++) {
+      int lineLength = generations[y].length;
+      for (int x = 0; x < lineLength; x++) {
+        int num = numLivingNeighbours(x, y);
+        if (isAlive(x, y)) {
+
+          if (num == 2 || num == 3)
+            nextGeneration[x][y] = true;
+          else
+            nextGeneration[x][y] = false;
+        } else {
+          if (num == 3)
+            nextGeneration[x][y] = true;
+          else
+            nextGeneration[x][y] = false;
+
+        }
+      }
+    }
+    return new Life(nextGeneration);
+
+  }
+
+  public int numLivingNeighbours(int x, int y) {
+    int counter = 0;
+    if (isAlive(x, y - 1))
+      counter++;
+    if (isAlive(x + 1, y - 1))
+      counter++;
+    if (isAlive(x + 1, y))
+      counter++;
+    if (isAlive(x + 1, y + 1))
+      counter++;
+    if (isAlive(x, y + 1))
+      counter++;
+    if (isAlive(x - 1, y + 1))
+      counter++;
+    if (isAlive(x - 1, y))
+      counter++;
+    if (isAlive(x - 1, y - 1))
+      counter++;
+    return counter;
   }
 }
--- LifeTest.java
+++ LifeTest.java
@@ -22,16 +22,51 @@ public class LifeTest {
 
     @Test
     public void destroyLonelyCell() {
+       // Arrange: zwei lebende Zellen
+        Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit einem Nachbarn sollte jetzt nicht mehr leben
+        assertTrue(!nextGen.isAlive(0, 1));
     }
 
 
     @Test
     public void keepAliveCell() {
+       // Arrange: drei lebende Zellen
+        Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit zwei Nachbarn sollte leben
+        assertTrue(nextGen.isAlive(0, 1));
     }
 
 
     @Test
     public void destroyCrowdedCell() {
+       // Arrange: fünf lebende Zellen
+        Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+        l.setAlive(1, 0);
+        l.setAlive(1, 1);
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit vier Nachbarn sollte jetzt nicht mehr leben
+        assertTrue(!nextGen.isAlive(0, 1));
     }
 
 
